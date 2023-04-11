a50f6ba08a55d6968f63a91f88ce876dd36b0044
Kichuaswith <130164918+Kichuaswith@users.noreply.github.com>
2023-04-11 20:55:43 +0530

    Update settings.py

--- todo/todoproject/todoproject/settings.py
+++ todo/todoproject/todoproject/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Kichuaswith$tododatabase',
-        'USER':'Kichuaswith'.
+        'USER':'Kichuaswith',
         'PASSWORD':'Kichu@97',
         'HOST':'Kichuaswith.mysql.pythonanywhere-services.com',
     }
